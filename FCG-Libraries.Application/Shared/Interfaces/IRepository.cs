f9e5a49502e45bb778d2e3ba9fc548e790c1d041
sorza <alexandre.sorza@outlook.com>
2025-11-12 16:27:04 -0300

    Criando classes de resultado e criando interface generica

---
+++ FCG-Libraries.Application/Shared/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using FCG_Libraries.Domain.Shared;
+
+namespace FCG_Libraries.Application.Shared.Interfaces
+{
+    public interface IRepository<T> where T : Entity
+    {
+        Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
